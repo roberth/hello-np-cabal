debc785678b4d68b245bd662726ec9f95419cce8
Robert Hensing <robert@roberthensing.nl>
2017-03-11 10:50:12 +0100

    Change .nix-project-cache to .nix-project/cache
    Issue https://github.com/roberth/np/issues/8

--- default.nix
+++ default.nix
@@ -7,6 +7,6 @@ let modifySrc = pkg: src: pkgs.haskell.lib.overrideCabal pkg (drv: {
       version = drv.version + "-modified";
       });
 
-    myFilterSource = builtins.filterSource (path: type: !(builtins.any (x: x == baseNameOf path) [".git" "default.nix" "shell.nix" ".nix-project-cache"]));
+    myFilterSource = builtins.filterSource (path: type: !(builtins.any (x: x == baseNameOf path) [".git" "default.nix" "shell.nix" ".nix-project"]));
 
 in modifySrc (pkgs.haskellPackages.callPackage ./hello-np-cabal.nix {}) (myFilterSource ./.)
